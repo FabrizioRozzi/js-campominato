a595d2170813288bd173b47e689ade34c25b1b45
FabrizioRozzi <rozzi458@gmail.com>
2021-04-15 16:54:13 +0200

    Finito

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,37 +5,40 @@ var punteggio = 0;
 
 var scelta_diff = parseInt(prompt("Inserisci la difficolta: 0, 1, 2"));
 switch (scelta_diff){
-  case (scelta_diff == 0):
+  case 0:
     arrBombe = calcolo_bombe( 100, 1);
-    arrUtente = calcolo_vittorie( 100, 1);
+    console.log(arrBombe);
+    calcolo_vittorie( 100, 1, arrBombe);
     break;
 
-  case (scelta_diff == 1):
+  case 1:
     arrBombe = calcolo_bombe( 80, 1);
-    arrUtente = calcolo_vittorie( 80, 1);
+    console.log(arrBombe);
+    arrUtente = calcolo_vittorie( 80, 1, arrBombe);
     break;
 
-  case (scelta_diff == 2):
+  case 2:
     arrBombe = calcolo_bombe( 50, 1);
-    arrUtente = calcolo_vittorie( 50, 1);
+    arrUtente = calcolo_vittorie( 50, 1, arrBombe);
     break;
 
-  case (scelta_diff > 2):
-    alert("Riavvia partita la difficolta inserita non é valida")
-    break  
+  // case   2:
+  //   alert("Riavvia partita la difficolta inserita non é valida")
+  //   break  
   
   
   }
 
-
+console.log(arrBombe)
 
 
 
 // Creo una funzione inserire, con un ciclo, for in maniera randomica 16 numeri da 1 a 100 in un array al computer.
 function calcolo_bombe( max, min){
   var i = 1;
+  var valoreRandom = 0;
+  var arrRandom = [];
   while(i <= 16 ){
-    var valoreRandom = 0;
     valoreRandom = Math.floor(Math.random() * (max - min)) + min;
     // Controllo con un if se il valore estratto é gia stato salvato in precedenza. 
     if(arrRandom.includes(valoreRandom)){
@@ -43,37 +46,37 @@ function calcolo_bombe( max, min){
     }else{
       // Se non é un duplicato lo pusho nell'array delle bombe e incremento la "i".
       arrRandom.push(valoreRandom);
-      console.log(arrRandom); 
       i++; 
     }
-  }
-}
+  }return arrRandom;
+ }
 
 // Creo una funzione per far inserire all'utente i numeri su cui faccio i controlli dovuti.
-function calcolo_vittorie( max, min){
+ function calcolo_vittorie( max, min, arrRandom){
 
   // Inizializzo un contatore per il punteggio e un flag per uscire dal ciclo.
   var flag = false;
   var counter = 0;
-
+  var arrGiocatore = [];
+  var valoreUtente = 0;
   // Apro un ciclo che ci chiude quando finisce la partita.
   while(flag == false ){
-    valoreUtente = parseInt(prompt("Inserisci un numero da " + min + " a " + min));
-
+    valoreUtente = parseInt(prompt("Inserisci un numero da " + min + " a " + max));
+    
     // Controlli del numero inserito
-    if(valoreUtente < min || valoreUtente > max){
+    if(valoreUtente <= min || valoreUtente >= max){
       alert("Il numero inserito non rispetta i canoni.")
-    }else if(arrRandom.includes(valoreUtente)){
+    }else if(arrGiocatore.includes(valoreUtente)){
       alert("Il numero scelto é gia stato inserito.")
     }else if(isNaN(valoreUtente) === true){
       alert("Il numero inserito non é un numero.")
     }else if(arrRandom.includes(valoreUtente)){
       flag = true;
-      
+      console.log("Hai trovato la bomba hai perso")
     }else{
       arrGiocatore.push(valoreUtente);
-      console.log(arrGiocatore); 
-      i++; 
+      console.log(arrGiocatore)
+      
       counter ++;
     }
   }
